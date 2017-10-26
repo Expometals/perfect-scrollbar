926eea2240160a459090a6af0efc167285ca34fe
Expometals <programmazione.expometals@gmail.com>
2017-10-26 14:17:16 +0200

    Update perfect-scrollbar.common.js

--- dist/perfect-scrollbar.common.js
+++ dist/perfect-scrollbar.common.js
@@ -309,13 +309,13 @@ function outerWidth(element) {
   );
 }
 
+var _isWebKit =  typeof document !== 'undefined' ? 'WebkitAppearance' in _document.documentElement.style : false;
+var _supportsTouch = typeof window !== 'undefined' ? ('ontouchstart' in window ||
+(window.DocumentTouch && document instanceof window.DocumentTouch)) : false;
+
 var env = {
-  isWebKit: document && 'WebkitAppearance' in document.documentElement.style,
-  supportsTouch:
-    window &&
-    ('ontouchstart' in window ||
-      (window.DocumentTouch && document instanceof window.DocumentTouch)),
-  supportsIePointer: navigator && navigator.msMaxTouchPoints,
+  isWebKit: _isWebKit,
+  supportsTouch: _supportsTouch,
 };
 
 var updateGeometry = function(i) {
